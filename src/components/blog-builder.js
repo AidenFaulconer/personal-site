55c19acfc0cdd727876d2a103d5ac5f156c6350a
Aiden Faulconer <aidenf09@yahoo.com>
2020-05-26 10:45:32 +1000

    style tweaks
    refactoring code and style tweaks

--- src/components/blog-builder.js
+++ src/components/blog-builder.js
@@ -2,77 +2,17 @@ import React, { useEffect, useState, useCallback } from "react";
 import { StaticQuery, Link, graphql } from "gatsby";
 import { CSSTransition } from "react-transition-group";
 import { blogSvgs } from "../../static/svg/hardcoded-svgs";
+import clickDragUtil from "./util/click-drag";
+import sideWaysScrollUtil from "./util/sideways-scroll";
 
 export const postContainer = "blog__wrapper";
 
 // TODO: add a contentID that refs a blog article, or find a better workaround here
 // TODO: dont dangerously set inner html to avoid injeciton vunerabilities, write a script to pre-process it in nodejs rather than in react
 export default React.memo(() => {
-  const enableSidewayScrolling = useCallback(container => {
-    const scrollHorizontally = e => {
-      e = window.event || e;
-      const delta = Math.max(-1, Math.min(1, e.wheelDelta || -e.detail)) * 2;
-      Object.keys(document.getElementsByClassName(container)).map(
-        elem => (elements[elem].scrollLeft -= delta * 40)
-      ); // Multiplied by 40
-      e.preventDefault();
-    };
-    let elements = document.getElementsByClassName(container);
-    if (elements.length > 0) {
-      // IE9, Chrome, Safari, Opera
-      Object.keys(elements).map(elem =>
-        elements[elem].addEventListener("mousewheel", scrollHorizontally, false)
-      );
-      // Firefox
-      Object.keys(elements).map(elem =>
-        elements[elem].addEventListener(
-          "DOMMouseScroll",
-          scrollHorizontally,
-          false
-        )
-      );
-    } else {
-      // IE 6/7/8
-      Object.keys(elements).map(elem =>
-        elements[elem]
-          .getElementsByClassName(container)
-          .attachEvent("onmousewheel", scrollHorizontally)
-      );
-    }
-  });
+  const enableSidewayScrolling = useCallback(sideWaysScrollUtil);
 
-  const clickDrag = useCallback(container => {
-    const slider = document.getElementsByClassName(container);
-    let isDown = false;
-    let startX;
-    let scrollLeft;
-    Object.keys(slider).map(elem => {
-      elem = slider[elem];
-
-      elem.addEventListener("mousedown", e => {
-        isDown = true;
-        elem.classList.add("click-active");
-        startX = e.pageX - elem.offsetLeft;
-        scrollLeft = elem.scrollLeft;
-      });
-      elem.addEventListener("mouseleave", () => {
-        isDown = false;
-        elem.classList.remove("click-active");
-      });
-      elem.addEventListener("mouseup", () => {
-        isDown = false;
-        elem.classList.remove("click-active");
-      });
-      elem.addEventListener("mousemove", e => {
-        if (!isDown) return;
-        // e.preventDefault()
-        e.stopPropagation();
-        const x = e.pageX - elem.offsetLeft;
-        const walk = (x - startX) * 3; // scroll-fast
-        elem.scrollLeft = scrollLeft - walk;
-      });
-    });
-  });
+  const clickDrag = useCallback(clickDragUtil);
 
   const [inProp, setInProp] = useState(false);
   useEffect(() => {
--- src/components/section-builder.js
+++ src/components/section-builder.js
@@ -7,44 +7,14 @@ import React, {
 } from "react";
 import { StaticQuery, Link, graphql } from "gatsby";
 // import ProjectViewer from "./project-viewer";
+import clickDragUtil from './util/click-drag'
 
 export const postContainer = "section__grid";
 
 // TODO: add a contentID that refs a blog article, or find a better workaround here
 // TODO: dont dangerously set inner html to avoid injeciton vunerabilities, write a script to pre-process it in nodejs rather than in react
 export default React.memo(() => {
-  const clickDrag = useCallback(container => {
-    const slider = document.getElementsByClassName(container);
-    let isDown = false;
-    let startX;
-    let scrollLeft;
-    Object.keys(slider).map(elem => {
-      elem = slider[elem];
-
-      elem.addEventListener("mousedown", e => {
-        isDown = true;
-        elem.classList.add("click-active");
-        startX = e.pageX - elem.offsetLeft;
-        scrollLeft = elem.scrollLeft;
-      });
-      elem.addEventListener("mouseleave", () => {
-        isDown = false;
-        elem.classList.remove("click-active");
-      });
-      elem.addEventListener("mouseup", () => {
-        isDown = false;
-        elem.classList.remove("click-active");
-      });
-      elem.addEventListener("mousemove", e => {
-        if (!isDown) return;
-        // e.preventDefault()
-        e.stopPropagation();
-        const x = e.pageX - elem.offsetLeft;
-        const walk = (x - startX) * 3; // scroll-fast
-        elem.scrollLeft = scrollLeft - walk;
-      });
-    });
-  });
+  const clickDrag = useCallback(clickDragUtil);
 
   const [inProp, setInProp] = useState(false);
   useEffect(() => {
